84f9c28a678cff7eedb0a4a134ff1537fcdae68e
Tuan Anh <tuan73176@gmail.com>
2026-09-19 18:42:01 +0000

    oppen-user 14 UKG3.250803.001 20251124 release-keys
    Change-Id: I0deca4926675cf17e9f8642683ac21c892ff5de5

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tifa)
+
+$(call add-radio-file-sha1-checked,radio/bootloader.img,35960fa9e0f2ababeb267d3ca1e5a6ac9b0e5916)
+
+endif
